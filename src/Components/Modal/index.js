3a1222b2cef28298d45779239ac756a57cf663ac
Günsel Akkemik <GUNSELAKKEMIK@HOTMAIL.COM.TR>
2022-08-12 12:42:19 +0300

    Added chakra and styles

---
+++ src/Components/Modal/index.js
@@ -0,0 +1,54 @@
+import {
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton, Button, Modal,
+} from '@chakra-ui/react'
+import PropTypes from "prop-types";
+
+const CustomModal = ({title, isOpen, onClose, children, onAction, actionText}) => {
+
+  const handleActionButton = () => {
+    onAction();
+    onClose();
+  }
+
+  return (
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {children}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Kapat
+            </Button>
+            {onAction && (
+                <Button colorScheme='teal' variant='outline' onClick={handleActionButton}>{actionText}</Button>
+            )}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+  );
+};
+
+CustomModal.propTypes = {
+  title: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  onAction: PropTypes.func,
+  actionText: PropTypes.string,
+}
+
+CustomModal.defaultProps = {
+  onAction: null,
+  actionText: "",
+}
+
+export default CustomModal;
---
+++ src/Providers/theme-provider.js
@@ -0,0 +1,101 @@
+import styled ,{ ThemeProvider, createGlobalStyle } from "styled-components";
+import InterLight from "../assets/fonts/Inter/Inter-Light-BETA.otf";
+import InterBold from "../assets/fonts/Inter/Inter-Bold.otf";
+import Inter from "../assets/fonts/Inter/Inter-Regular.otf";
+import InterItalic from "../assets/fonts/Inter/Inter-Italic.otf";
+import InterSemibold from "../assets/fonts/Inter/Inter-SemiBold.otf"
+
+const MainContainer = styled.div`
+  width:100vw;
+  height: 100vh;
+  background: ${({ theme }) => theme.backgrounds.mainBackground};
+  transition: all .5s;
+`;
+
+const theme = {
+  colors: {
+    cardTextColor: "#000000",
+    cardColor: "#ffffff",
+    gray: "#757575",
+    white: "#FFFFFF",
+    black: "#000000",
+    noBgTextColor: "#000000",
+    borderColor: "rgba(210,210,210,0.41)",
+    successColor: "#28A745",
+    dangerColor: "#DC3545",
+    warningColor: "#E0A800",
+    infoColor: "#17A2B8",
+    transparentBlack: "rgba(0,0,0,0.4)"
+  },
+  backgrounds: {
+    mainBackground: "#1A202C",
+    modalBackground: "#2D3748",
+    headerBackground: "#151515",
+    darkBackground: "#102738",
+    copsBackground: "#C4C4C4",
+    wantedBackground: "#1A2540",
+    inputBackground: "#091e2a",
+    lightBackground: "#23405E",
+  },
+  borderColors: {
+    grayBorder: "#b4b4b4",
+    darkGray: "#6a6a6a"
+  },
+  fonts: {
+    regular: "Interregular",
+    light: "Interlight",
+    bold: "Interbold",
+    italic: "Interitalic",
+    semibold: "Intersemibold",
+  },
+  fontSizes: {
+    extraSmall: "10px",
+    tiny: "12px",
+    small: "14px",
+    medium: "16px",
+    large: "18px",
+    extraLarge: "20px",
+    titleLarge: "26px"
+  },
+};
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Interregular';
+    src: url(${Inter});
+  }
+  @font-face {
+    font-family: 'Interlight';
+    src: url(${InterLight});
+  }
+  @font-face {
+    font-family: 'Interbold';
+    src: url(${InterBold});
+  }
+  @font-face {
+    font-family: 'Interitalic';
+    src: url(${InterItalic});
+  }
+  @font-face {
+    font-family: 'InterSemibold';
+    src: url(${InterSemibold});
+  }
+  body {
+    font-family: ${({ theme }) => theme.fonts.regular};
+    margin: 0;
+  }
+`;
+
+const MainThemeProvider = ({ children }) => {
+
+  return (
+      <ThemeProvider theme={theme}>
+        <MainContainer>
+          <GlobalStyle />
+          {children}
+        </MainContainer>
+      </ThemeProvider>
+  );
+};
+
+export default MainThemeProvider;
